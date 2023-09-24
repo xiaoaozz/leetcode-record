22344a70bf181b727898c99d711263e2f1e97dc4
zal <3189137314@qq.com>
2023-09-24 22:26:47 +0800

    feat：0918每日灵茶-待补

---
+++ src/main/java/每日灵茶/_20230918/Main.java
@@ -0,0 +1,19 @@
+package 每日灵茶._20230918;
+
+/**
+ * @Author zal
+ * @Date 2023/9/18 13:04
+ * @Description https://atcoder.jp/contests/abc248/tasks/abc248_d
+ * @Version 1.0
+ * <p>
+ * 题面：输入 n(1≤n≤2e5) 和长为 n 的数组 a(1≤a[i]≤n)，下标从 1 开始。
+ * 然后输入 q 个询问，每个询问输入 L R (1≤L≤R≤n) 和 x(1≤x≤n)。
+ * 对每个询问，输出有多少个下标 i 在 [L,R] 内的 a[i]，满足 a[i]=x。
+ * <p>
+ * 思路：
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
